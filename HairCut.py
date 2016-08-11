ec871dd05702d90adfa969917d133c8604498843
root <root@asb-dev-dataanalysis01.(none)>
2016-08-11 15:27:02 +0000

    Update for Posterity

--- HairCut.py
+++ HairCut.py
@@ -2,16 +2,22 @@ from __future__ import print_function
 from bs4 import BeautifulSoup
 from collections import defaultdict
 from flask import Flask, jsonify, make_response, abort, url_for, request
+from Harvard import Excel2CSV
 from os.path import join, dirname, abspath
 from IPython.display import HTML
-import Harvard
+import csv
+import glob
 import numpy as np
+import os
 import re
 import requests
 import time
+import urllib2
 import xlrd
+import xlsxwriter
 import xlwt
 
+delMe = 0
 w=1
 x=2
 y=3
@@ -22,19 +28,35 @@ z=4
 
 book = xlwt.Workbook(encoding="utf-8")
 
-sheet1 = book.add_sheet("Results")
+sheet1 = book.add_sheet("Results", cell_overwrite_ok=True)
 sheet1.write(0,0, "Telephone Number")
-
-occurences = defaultdict(int)
 
 url = "http://800notes.com/Phone.aspx/*"
 headers = {'User-Agent': 'Chrome/39.0.2171.95 Safari/537.36 AppleWebKit/537.36 (KHTML, like Gecko)'}
 
 response = requests.get(url, headers=headers)
 content = BeautifulSoup(response.content, "lxml")
-fileName = raw_input("Input the excel file with extension\n>")
-fname = join(dirname(abspath('__file__')), '%s' % fileName)
+fileName = raw_input("Input the file with extension\n>")
+stopPoint = fileName.index('.')
+prepRev = fileName[stopPoint:]
+csvTest = prepRev
 
+if csvTest == ".csv":
+   excelFile = xlsxwriter.Workbook(fileName + '.xlsx')
+   worksheet = excelFile.add_worksheet()
+   with open(fileName,'rb') as f:
+      content = csv.reader(f)
+      for index_col, data_in_col in enumerate(content):
+            for index_row, data_in_cell in enumerate(data_in_col):
+                  worksheet.write(index_col,index_row,data_in_cell)
+   excelFile.close()
+   fileName = (fileName + '.xlsx')
+   delMe = 1
+   print("Temporary Convert to xlsx done.\n")
+
+deleteFile = fileName
+
+fname = join(dirname(abspath('__file__')), '%s' % fileName)
 #http://800notes.com/Phone.aspx/*-***-***-**** is the layout#
 
 xl_workbook = xlrd.open_workbook(fname)
@@ -44,58 +66,67 @@ xl_sheet = xl_workbook.sheet_by_name(sheet_names[0])
 website = raw_input("Input 1 for 800Notes, input 2 for BBB\n>")
 
 if(website =="1"):
-        sheet1.write(0,1, "# of Messages")
-        sheet1.write(0,2, "Category")
-        sheet1.write(0,3, "Last 3 Messages")         
+   sheet1.write(0,1, "# of Messages")
+   sheet1.write(0,2, "Category")
+   sheet1.write(0,3, "Last 3 Messages")         
 
 if(website == "2"):
-        sheet1.write(0,1, "Acreditted") 
+   sheet1.write(0,1, "Acreditted") 
 
 col = xl_sheet.col_slice(0,1,10101010)
 from xlrd.sheet import ctype_text
 #print('(Column #) type:value')
 for idx, cell_obj in enumerate(col):
-        cell_type_str = ctype_text.get(cell_obj.ctype, 'unknown type')  
-        cell_obj_str = str(cell_obj)
+   cell_type_str = ctype_text.get(cell_obj.ctype, 'unknown type')  
+   cell_obj_str = str(cell_obj)
    
-        tele800 = (cell_obj_str[7] + "-" + cell_obj_str[8:11] + "-" + cell_obj_str[11:14] + "-" + cell_obj_str[14:18])
-        teleBBB = ("%28" + cell_obj_str[8:11] + "%29+" + cell_obj_str[11:14] + "-" + cell_obj_str[14:18])
-        print('(%s) %s' % (idx, tele800))
-        w = tele800
-        sheet1.write(idx+1, 0, w)
+   tele800 = (cell_obj_str[7] + "-" + cell_obj_str[8:11] + "-" + cell_obj_str[11:14] + "-" + cell_obj_str[14:18])
+   teleBBB = ("%28" + cell_obj_str[8:11] + "%29+" + cell_obj_str[11:14] + "-" + cell_obj_str[14:18])
+   print('(%s) %s' % (idx, tele800))
+   w = tele800
+   sheet1.write(idx+1, 0, w)
     
-        if(website == "1"):
-                reqInput = ('http://800notes.com/Phone.aspx/%s' % (tele800))
-                print (reqInput)
-                time.sleep(3)
-                requestRec = requests.get(reqInput)
-                soup = BeautifulSoup(requestRec.content, "lxml")
-                print(requestRec.content)
-                callType = soup.find('div',{'class':"callDetails"})
-                print(callType)
-                if callType is not (None):
-                        callCata = callType[10:]
-                        sheet1.write(idx+1, 2, callCata)
+   if(website == "1"):
+      reqInput = ('http://800notes.com/Phone.aspx/%s' % (tele800))
+      print (reqInput)
+      time.sleep(3)
+      requestRec = requests.get(reqInput)
+      soup = BeautifulSoup(requestRec.content, "lxml")
+      print(requestRec.content)
+      callType = soup.find('div',{'class':"callDetails"})
+      print(callType)
+      if callType is not (None):
+         callCata = callType[10:]
+         sheet1.write(idx+1, 2, callCata)
                 
-                stopPoint = fileName.index('.')
-                prepRev = fileName[0:stopPoint]
-                book.save(prepRev + "_rev_800.xls")        
-                #x = bs4.divtotalmess
+      stopPoint = fileName.index('.')
+      prepRev = fileName[0:stopPoint]
+      totalName = prepRev + "_rev_800.xls"  
+      book.save(totalName)
+      #x = bs4.divtotalmess
         
-        if(website == "2"):
-                reqInput = ('https://www.bbb.org/search/?splashPage=true&type=name&input='+ teleBBB +'&location=&tobid=&filter=business&radius=&country=USA%2CCAN&language=en&codeType=YPPA')
-                print (reqInput)
-                time.sleep(2)
-                requestRec = requests.get(reqInput)
-                soup = BeautifulSoup(requestRec.content,"lxml")
-                divTags = soup.find('class')
-                Badge = soup.find_all('img',{'class':'badge-accredited'})
-                if len(Badge)!=0:
-                        sheet1.write(idx+1,1,"Is Accredited")
-         
-                stopPoint = fileName.index('.')
-                prepRev = fileName[0:stopPoint]
-                book.save(prepRev + "_rev_BBB.xls")
-               
+   if(website == "2"):
+      reqInput = ('https://www.bbb.org/search/?splashPage=true&type=name&input='+ teleBBB +'&location=&tobid=&filter=business&radius=&country=USA%2CCAN&language=en&codeType=YPPA')
+      print (reqInput)
+      time.sleep(2)
+      requestRec = requests.get(reqInput)
+      soup = BeautifulSoup(requestRec.content,"lxml")
+      divTags = soup.find('class')
+      Badge = soup.find_all('img',{'class':'badge-accredited'})
+      Hit = soup.find_all('td',{'class':'accredited'})
+      if len(Hit)!=0:
+         sheet1.write(idx+1,1,"Got a Hit")      
+      if len(Badge)!=0:
+         sheet1.write(idx+1,1,"Is Accredited")
+      stopPoint = fileName.index('.')
+      prepRev = fileName[0:stopPoint]
+      totalName = prepRev + "_rev_BBB.xls"
+      book.save(totalName)
+
+Excel2CSV(totalName, "Results", prepRev + '.csv')
+
+if delMe == 1:
+   os.remove(deleteFile)
+   print("Temp File Cleaned!\n")
 
 print("Ding! All done!")
--- Harvard.py
+++ Harvard.py
@@ -1,5 +1,7 @@
 #https://blogs.harvard.edu/rprasad/2014/06/16/reading-excel-with-python-xlrd/#
 import xlwt
+import xlrd
+import csv
 from HTMLParser import HTMLParser
 
 def construct_entries():
@@ -31,3 +33,18 @@ def get_comment_number(entry):
 
 def get_comment_content(entry):
     return entry.find('div', {"class": "oos_previewBody"}).getText()
+
+def Excel2CSV(ExcelFile, SheetName, CSVFile):
+    import xlrd
+    import csv
+    workbook = xlrd.open_workbook(ExcelFile)
+    worksheet = workbook.sheet_by_name(SheetName)
+    csvfile = open(CSVFile, 'wb')
+    wr = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+
+    for rownum in xrange(worksheet.nrows):
+        wr.writerow(
+            list(x.encode('utf-8') if type(x) == type(u'') else x
+                 for x in worksheet.row_values(rownum)))
+
+    csvfile.close()
