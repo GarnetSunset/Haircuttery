f80f3f29743ba46b360c6ffdc9dcc91aaa2fa693
root <root@asb-dev-dataanalysis01.(none)>
2016-08-22 20:26:01 +0000

    First Commit

--- HairCut.py
+++ HairCut.py
@@ -2,9 +2,10 @@ from __future__ import print_function
 from bs4 import BeautifulSoup
 from collections import defaultdict
 from flask import Flask, jsonify, make_response, abort, url_for, request
+from googleapiclient import sample_tools
 from Harvard import Excel2CSV
-from os.path import join, dirname, abspath
 from IPython.display import HTML
+from os.path import join, dirname, abspath
 import csv
 import glob
 import numpy as np
@@ -28,15 +29,14 @@ z=4
 
 book = xlwt.Workbook(encoding="utf-8")
 
-sheet1 = book.add_sheet("Results", cell_overwrite_ok=True)
-sheet1.write(0,0, "Telephone Number")
+worksheet = book.add_sheet("Results", cell_overwrite_ok=True)
 
 url = "http://800notes.com/Phone.aspx/*"
 headers = {'User-Agent': 'Chrome/39.0.2171.95 Safari/537.36 AppleWebKit/537.36 (KHTML, like Gecko)'}
 
 response = requests.get(url, headers=headers)
 content = BeautifulSoup(response.content, "lxml")
-fileName = raw_input("Input the file with extension\n>")
+fileName = raw_input("\nInput the file with extension\n>")
 stopPoint = fileName.index('.')
 prepRev = fileName[stopPoint:]
 csvTest = prepRev
@@ -63,15 +63,29 @@ xl_workbook = xlrd.open_workbook(fname)
 sheet_names = xl_workbook.sheet_names()
 xl_sheet = xl_workbook.sheet_by_name(sheet_names[0])
 
-website = raw_input("Input 1 for 800Notes, input 2 for BBB\n>")
+website = raw_input("Input 1 for 800Notes Google results, input 2 for BBB\n>")
 
 if(website =="1"):
-   sheet1.write(0,1, "# of Messages")
-   sheet1.write(0,2, "Category")
-   sheet1.write(0,3, "Last 3 Messages")         
+   stopPoint = fileName.index('.')
+   prepRev = fileName[0:stopPoint]
+   totalName = prepRev + "_rev_800.xlsx"   
+   workbook = xlsxwriter.Workbook(totalName)
+   worksheet = workbook.add_worksheet()   
+   worksheet.write(0,0, "Telephone Number")
+   worksheet.write(0,1, "# of Messages")
+   worksheet.write(0,2, "Category")
+   workbook.write(0,3, "Last 3 Messages")
 
 if(website == "2"):
-   sheet1.write(0,1, "Acreditted") 
+   stopPoint = fileName.index('.')
+   prepRev = fileName[0:stopPoint]
+   totalName = prepRev + "_rev_BBB.xlsx"
+   workbook = xlsxwriter.Workbook(totalName)
+   worksheet = workbook.add_worksheet()
+   worksheet.write(0,0, "Telephone Number")
+   worksheet.write(0,1, "Acreditted")
+
+worksheet.set_column('A:A',13)
 
 col = xl_sheet.col_slice(0,1,10101010)
 from xlrd.sheet import ctype_text
@@ -80,50 +94,42 @@ for idx, cell_obj in enumerate(col):
    cell_type_str = ctype_text.get(cell_obj.ctype, 'unknown type')  
    cell_obj_str = str(cell_obj)
    
-   tele800 = (cell_obj_str[7] + "-" + cell_obj_str[8:11] + "-" + cell_obj_str[11:14] + "-" + cell_obj_str[14:18])
+   tele800 = (cell_obj_str[8:11] + "-" + cell_obj_str[11:14] + "-" + cell_obj_str[14:18])
    teleBBB = ("%28" + cell_obj_str[8:11] + "%29+" + cell_obj_str[11:14] + "-" + cell_obj_str[14:18])
    print('(%s) %s' % (idx, tele800))
-   w = tele800
-   sheet1.write(idx+1, 0, w)
+   perm = tele800
+   site800 = tele800 + " site:800notes.com"
+   worksheet.write(idx+1, 0, perm)
     
-   if(website == "1"):
-      reqInput = ('http://800notes.com/Phone.aspx/%s' % (tele800))
+   if(website == "1"):  
+      reqInput = ('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s+site:800notes.com&rsz=large' % (tele800))
       print (reqInput)
-      time.sleep(3)
+      time.sleep(10)
       requestRec = requests.get(reqInput)
       soup = BeautifulSoup(requestRec.content, "lxml")
       print(requestRec.content)
-      callType = soup.find('div',{'class':"callDetails"})
-      print(callType)
-      if callType is not (None):
-         callCata = callType[10:]
-         sheet1.write(idx+1, 2, callCata)
-                
-      stopPoint = fileName.index('.')
-      prepRev = fileName[0:stopPoint]
-      totalName = prepRev + "_rev_800.xls"  
-      book.save(totalName)
-      #x = bs4.divtotalmess
+      noMatch = soup.body.findAll(text='did not match any documents.')
+      print(noMatch)
+      if len(noMatch)==0:
+         worksheet.write(idx+1, 2, "Got a hit")     
         
    if(website == "2"):
       reqInput = ('https://www.bbb.org/search/?splashPage=true&type=name&input='+ teleBBB +'&location=&tobid=&filter=business&radius=&country=USA%2CCAN&language=en&codeType=YPPA')
       print (reqInput)
-      time.sleep(2)
+      time.sleep(1)
       requestRec = requests.get(reqInput)
       soup = BeautifulSoup(requestRec.content,"lxml")
       divTags = soup.find('class')
       Badge = soup.find_all('img',{'class':'badge-accredited'})
       Hit = soup.find_all('td',{'class':'accredited'})
       if len(Hit)!=0:
-         sheet1.write(idx+1,1,"Got a Hit")      
+         worksheet.write(idx+1,1,"Got a Hit")      
       if len(Badge)!=0:
-         sheet1.write(idx+1,1,"Is Accredited")
-      stopPoint = fileName.index('.')
-      prepRev = fileName[0:stopPoint]
-      totalName = prepRev + "_rev_BBB.xls"
-      book.save(totalName)
+         worksheet.write(idx+1,1,"Is Accredited")
+         
+workbook.close()
 
-Excel2CSV(totalName, "Results", prepRev + '.csv')
+Excel2CSV(totalName, "Sheet1", prepRev + '.csv')
 
 if delMe == 1:
    os.remove(deleteFile)
