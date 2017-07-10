cb846a3ddb387e3d1d8ba08c1318fb6aaffa6e0f
James Duarte <GarnetSunset@users.noreply.github.com>
2017-07-10 16:48:38 -0400

    Documentation and a Lil Cleanup!

--- HairCut.py
+++ HairCut.py
@@ -28,22 +28,49 @@ import xlrd
 import xlsxwriter
 import xlwt
 
+# Wait out my mistake.
+
+
+def blocked():
+    block = soup.find(text=re.compile(r"OctoNet HTTP filter"))
+    extrablock = soup.find(text=re.compile(
+        r"returning an unknown error"))
+    extrablock = soup.find(
+        text=re.compile(r"Gateway time-out"))
+    type(block) is str
+    type(extrablock) is str
+    if(block is not None or extrablock is not None):
+        print("\n Ugh. I'm gonna go talk to the host of the site real quick. Should take an hour or two.")
+        time.sleep(7200)
+
+# Break if no chromedriver.
+
+
+def breaker():
+    done = True
+    print("\nPlease refer to the Readme, you don't have chromedriver.exe in 'C:\chromedriver'")
+    time.sleep(15)
+    sys.exit()
+
+# Loading Animation that plays when the user is running a file.
+
 
 def loading():
     for s in itertools.cycle(['|', '/', '-', '\\']):
         if done:
             break
-        if(breaker == 1):
-            break
         sys.stdout.write('\rloading ' + s)
         sys.stdout.flush()
         time.sleep(0.1)
+
+# TimeoutHandler that takes care of webDriver fails.
 
 
 def TimeOutHandler(driver, webdriver, worksheet):
     driver.close()
     driver = webdriver.Chrome()
     worksheet.write(idx + 1, 7, "Timeout Exception")
+
 
 bbbEnd = "&locationText=&locationLatLng=&page=1"
 bbbUrl = "https://www.bbb.org/en/us/search?inputText="
@@ -54,67 +81,73 @@ debtCount = 0
 delMe = 0
 done = False
 hospitalCount = 0
+numFormat = "3"
 scamCount = 0
 spamCount = 0
-w = 1
-x = 2
-y = 3
-z = 4
 
+# Create a UTF-8 Workbook.
 book = xlwt.Workbook(encoding="utf-8")
+
+# Assign a User-Agent to python.
 headers = {
     'User-Agent': 'Chrome/39.0.2171.95 Safari/537.36 AppleWebKit/537.36 (KHTML, like Gecko)'}
-worksheet = book.add_sheet("Results", cell_overwrite_ok=True)
-file_paths = sys.argv[1:2]
-draganddrop = ''.join(file_paths)
-webType = sys.argv[2:3]
-dragNDrop2 = ''.join(webType)
-numType = sys.argv[3:4]
-dragNDrop3 = ''.join(numType)
 
-if draganddrop == "":
+# Create a worksheet named "Results".
+worksheet = book.add_sheet("Results", cell_overwrite_ok=True)
+
+# Join the dragged files to create strings.
+dragNDrop = ''.join(sys.argv[1:2])
+dragNDrop2 = ''.join(sys.argv[2:3])
+
+# Was a file dragged onto the Batch file?
+# If not the string "dragNDrop" will be empty and the user will be prompted.
+if dragNDrop == "":
     fileName = raw_input("\nInput the file with extension\n>")
 else:
-    fileOnly = draganddrop.rfind('\\') + 1
-    fileName = draganddrop[fileOnly:]
+    # Obtain the fileName only by removing the directory name.
+    fileOnly = dragNDrop.rfind('\\') + 1
+    fileName = dragNDrop[fileOnly:]
 
-stopPoint = fileName.index('.')
-prepRev = fileName[stopPoint:]
-preName = fileName[:stopPoint]
-
-reload(sys)
-sys.setdefaultencoding('utf')
-
-if prepRev == ".csv":
-    totalName = preName + '.xlsx'
-    excelFile = xlsxwriter.Workbook(totalName)
-    excelFile.close()
-    excelFile = xlsxwriter.Workbook(totalName)
-    worksheet = excelFile.add_worksheet()
-    enumColumn(fileName=fileName, worksheet=worksheet)
-    excelFile.close()
-    fileName = (preName + '.xlsx')
-    delMe = 1
-    print("Temporary Convert to xlsx done.\n")
-
-fname = join(dirname(abspath('__file__')), '%s' % fileName)
-
-xl_workbook = xlrd.open_workbook(fname)
-sheet_names = xl_workbook.sheet_names()
-xl_sheet = xl_workbook.sheet_by_name(sheet_names[0])
-
+# Was a site given in the Batch file?
+# If not the string "dragNDrop2" will be empty and the user will be prompted.
 if dragNDrop2 == "":
     website = raw_input(
         "Input 1 for whoscall.in results, input 2 for BBB, input 3 for 800Notes\n>")
 else:
     website = dragNDrop2
 
-if dragNDrop3 == "":
-    numFormat = "3"
-else:
-    numFormat = dragNDrop3
+# Find the period in the file, which determines the prepRev or extension, and the fileName.
+stopPoint = fileName.index('.')
+prepRev = fileName[stopPoint:]
+preName = fileName[:stopPoint]
 
-if(website == "v"):
+# Make sure we're still encoding in UTF. Don't want any mistakes now, do we?
+reload(sys)
+sys.setdefaultencoding('utf')
+
+# Is the extension CSV? If so we'll convert it to xlsx.
+if prepRev == ".csv":
+    totalName = preName + '.xlsx'
+    excelFile = xlsxwriter.Workbook(totalName)
+    excelFile.close()
+    # You must close the file because opening fails otherwise.
+    excelFile = xlsxwriter.Workbook(totalName)
+    worksheet = excelFile.add_worksheet()
+    enumColumn(fileName=fileName, worksheet=worksheet)
+    excelFile.close()
+    fileName = (preName + '.xlsx')
+    print("Temporary Convert to xlsx done.\n")
+
+# Get ready for XLRD to parse the original file (or the converted one).
+fname = join(dirname(abspath('__file__')), '%s' % fileName)
+
+# Parse it XLRD!
+xl_workbook = xlrd.open_workbook(fname)
+sheet_names = xl_workbook.sheet_names()
+xl_sheet = xl_workbook.sheet_by_name(sheet_names[0])
+
+# If the user types "d" for the website choice, they will be prompted again, but, this time given debug info.
+if(website == "d"):
     if os.name == 'nt':
         os.system('cls')
     else:
@@ -124,6 +157,7 @@ if(website == "v"):
     logging.basicConfig(level=logging.DEBUG)
     logging.debug('Only shown in debug mode')
 
+# Start the little spinny animation.
 g = threading.Thread(target=loading)
 g.start()
 
@@ -159,10 +193,7 @@ if(website == "2"):
         worksheet.write(0, 1, "Acreditted")
         siteType = "_rev_BBB.xlsx"
     else:
-        breaker = 1
-        print("\nPlease refer to the Readme, you don't have chromedriver.exe in 'C:\chromedriver'")
-        time.sleep(15)
-        sys.exit()
+        breaker()
 
 if(website == "3"):
     if(os.path.exists(r"C:/chromedriver.exe") or os.path.isfile('chromedriver.exe')):
@@ -186,22 +217,25 @@ if(website == "3"):
         worksheet.write(0, 7, "Sentiment")
         siteType = "_rev_800notes.xlsx"
     else:
-        breaker = 1
-        print("\nPlease refer to the Readme, you don't have chromedriver.exe in 'C:\chromedriver.exe'")
-        time.sleep(15)
-        sys.exit()
+        breaker()
 
+# Set column to A:A, the first column.
 worksheet.set_column('A:A', 13)
-col = xl_sheet.col_slice(0, 1, 10101010)
+# Read the slice from the first cell to the last accessible row in Excel.
+col = xl_sheet.col_slice(0, 1, 1048576)
+# Read each string line by line.
 for idx, cell_obj in enumerate(col):
     cell_type_str = ctype_text.get(cell_obj.ctype, 'unknown type')
     cell_obj_str = str(cell_obj)
 
+    # Does a dash, parenthesis, or none of those exist? That will decide the numFormat.
     if "-" in cell_obj_str:
         numFormat = "1"
 
     if "(" in cell_obj_str:
         numFormat = "2"
+
+    # Cut the numbers to their appropriate format.
 
     if(numFormat == "1"):
         firstStart = cell_obj_str.index('-') - 3
@@ -241,10 +275,11 @@ for idx, cell_obj in enumerate(col):
 
     worksheet.write(idx + 1, 0, "1" + teleWho)
 
+    # WhosCallinScrapes using the python Requests library. Nice and clean.
     if(website == "1"):
         reqInput = "http://whoscall.in/1/%s/" % (teleWho)
-        urlfile = urllib2.Request(reqInput)
         time.sleep(1)
+        urlfile = urllib2.Request(reqInput)
         requestRec = requests.get(reqInput)
         soup = BeautifulSoup(requestRec.content, "lxml")
         noMatch = soup.find(text=re.compile(
@@ -252,9 +287,13 @@ for idx, cell_obj in enumerate(col):
         type(noMatch) is str
         if noMatch is None:
             worksheet.write(idx + 1, 2, "Got a hit")
+
+            # Check for number of comments.
             howMany = soup.find_all('img', {'src': '/default-avatar.gif'})
             howManyAreThere = len(howMany)
             worksheet.write(idx + 1, 1, howManyAreThere)
+
+            # Search for text on the sites that indicates their sentiment and generate the top response.
             scamNum = [div for div in soup.find_all('div', {'style': 'font-size:14px; margin:10px; overflow:hidden'})
                        if 'scam' in div.text.lower() or 'Scam' in div.text.lower() or 'scams' in div.text.lower()]
             scamCount = len(scamNum)
@@ -267,26 +306,35 @@ for idx, cell_obj in enumerate(col):
             hospitalNum = [div for div in soup.find_all(
                 'div', {'style': 'font-size:14px; margin:10px; overflow:hidden'}) if 'hospital' in div.text.lower() or 'Hospital' in div.text.lower()]
             hospitalCount = len(hospitalNum)
-            if hospitalCount > 0:
-                hospitalCount + 9999
-            searchTerms = {'Scam': scamCount, 'Spam': spamCount,
-                           'Debt Collector': debtCount, 'Hospital': hospitalCount}
-            sentiment = max(searchTerms, key=searchTerms.get)
             worksheet.write(idx + 1, 3, scamCount)
             worksheet.write(idx + 1, 4, spamCount)
             worksheet.write(idx + 1, 5, debtCount)
             worksheet.write(idx + 1, 6, hospitalCount)
+
+            # Hospitals are important to look at, so I boost them.
+            if hospitalCount > 0:
+                hospitalCount + 9999
+
+            searchTerms = {'Scam': scamCount, 'Spam': spamCount,
+                           'Debt Collector': debtCount, 'Hospital': hospitalCount}
+            sentiment = max(searchTerms, key=searchTerms.get)
             worksheet.write(idx + 1, 7, sentiment)
+
+            # Stalemate?
             if scamCount == 0 and spamCount == 0 and debtCount == 0 and hospitalCount == 0:
                 worksheet.write(idx + 1, 7, "No Entries Detected")
 
     if(website == "2"):
-        driver.get(bbbUrl+
+
+        # Selenium, get that site for me! (bbbUrl + bbbEnd are defined above)
+        driver.get(bbbUrl +
                    teleBBB + bbbEnd)
         time.sleep(1)
         requestRec = driver.page_source
         soup = BeautifulSoup(requestRec, "lxml")
         Hit = soup.find_all('aside', {'class': 'search-result__aside'})
+
+        # Cloned the previous section, but, with changes to the URL via bbbUrlAC.
         driver.get(bbbUrlAC +
                    teleBBB + bbbEnd)
         requestRec = driver.page_source
@@ -298,27 +346,24 @@ for idx, cell_obj in enumerate(col):
             worksheet.write(idx + 1, 1, "Is Accredited")
 
     if(website == "3"):
+        # 800Notes, the big one.
         try:
             driver.get('http://800notes.com/Phone.aspx/%s' % (tele800))
         except TimeoutException as ex:
             TimeOutHandler(driver=driver, worksheet=worksheet,
                            webdriver=webdriver)
             break
-        delay = 4
-        time.sleep(4)
+        time.sleep(2)
         requestRec = driver.page_source
         soup = BeautifulSoup(requestRec, "lxml")
+
+        # This entry doesn't exist if this regex succeeds.
         noMatch = soup.find(text=re.compile(r"Report the call using the form"))
         soup.prettify()
         type(noMatch) is str
-        block = soup.find(text=re.compile(r"OctoNet HTTP filter"))
-        extrablock = soup.find(text=re.compile(r"returning an unknown error"))
-        extrablock = soup.find(text=re.compile(r"Gateway time-out"))
-        type(block) is str
-        type(extrablock) is str
-        if(block is not None or extrablock is not None):
-            print("\n Damn. Gimme an hour to fix this.")
-            time.sleep(5000)
+
+        # Make sure we don't get blocked, and if we do, wait it out.
+        blocked()
 
         if noMatch is None:
             try:
@@ -328,6 +373,7 @@ for idx, cell_obj in enumerate(col):
                 TimeOutHandler(driver=driver, worksheet=worksheet,
                                webdriver=webdriver)
                 break
+            blocked()
             curSite = driver.current_url
             pageExist = soup.find("a", class_="oos_i_thumbDown")
             type(pageExist) is str
@@ -346,7 +392,7 @@ for idx, cell_obj in enumerate(col):
             thumbsLen = len(thumbs)
             thumbPlus = thumbsLen + int(convertNum)
             worksheet.write(idx + 1, 1, thumbPlus)
-            delay = 3
+            time.sleep(2)
             if(pageExist is not None):
                 while(int(countitup) != int(pageNum) + 1):
                     try:
@@ -360,7 +406,6 @@ for idx, cell_obj in enumerate(col):
                         TimeOutHandler(
                             driver=driver, worksheet=worksheet, webdriver=webdriver)
                         break
-                    delay = 4
                     requestRec = driver.page_source
                     soup = BeautifulSoup(requestRec, "lxml")
                     countitup = int(countitup) + 1
@@ -380,25 +425,18 @@ for idx, cell_obj in enumerate(col):
                     spamCount = len(spamNum) + spamCount
                     debtCount = len(debtNum) + debtCount
                     hospitalCount = len(hospitalNum) + hospitalCount
-                    block = soup.find(text=re.compile(r"OctoNet HTTP filter"))
-                    extrablock = soup.find(text=re.compile(
-                        r"returning an unknown error"))
-                    extrablock = soup.find(
-                        text=re.compile(r"Gateway time-out"))
-                    type(block) is str
-                    type(extrablock) is str
-                    if(block is not None or extrablock is not None):
-                        print("\n Damn. Gimme an hour to fix this.")
-                        time.sleep(5000)
-                if hospitalCount > 0:
-                    hospitalCount + 9999
-                searchTerms = {'Scam': scamCount, 'Spam': spamCount,
-                               'Debt Collector': debtCount, 'Hospital': hospitalCount}
-                sentiment = max(searchTerms, key=searchTerms.get)
+                    blocked()
                 worksheet.write(idx + 1, 3, scamCount)
                 worksheet.write(idx + 1, 4, spamCount)
                 worksheet.write(idx + 1, 5, debtCount)
                 worksheet.write(idx + 1, 6, hospitalCount)
+
+                if hospitalCount > 0:
+                    hospitalCount + 9999
+
+                searchTerms = {'Scam': scamCount, 'Spam': spamCount,
+                               'Debt Collector': debtCount, 'Hospital': hospitalCount}
+                sentiment = max(searchTerms, key=searchTerms.get)
                 worksheet.write(idx + 1, 7, sentiment)
                 if scamCount == 0 and spamCount == 0 and debtCount == 0 and hospitalCount == 0:
                     worksheet.write(idx + 1, 7, "No Entries Detected")
@@ -409,26 +447,32 @@ for idx, cell_obj in enumerate(col):
             spamCount = 0
             worksheet.write(idx + 1, 2, int(pageNum))
 
+# Close up Shop!
 workbook.close()
 prepRev = preName + '_temp.csv'
 Excel2CSV(totalName, "Sheet1", prepRev)
 
-if draganddrop == "":
+# Determine if file was dragged or not for creation of Dirs.
+if dragNDrop == "":
     if not os.path.exists("WorkingDir"):
         os.makedirs("WorkingDir")
     if not os.path.exists("WorkingDir/" + preName):
         os.makedirs("WorkingDir/" + preName)
 
+# Was the file originially a CSV?
 if prepRev == ".csv":
     totalName = preName + prepRev
 else:
     totalName = preName + ".xlsx"
 
-if draganddrop == "":
+# If we haven't already moved all of the files, here we go.
+if dragNDrop == "":
     copyfile(totalName, "WorkingDir/" + preName + '/' + totalName)
-    move(preName + siteType, "WorkingDir/" + preName + '/' + preName + siteType)
+    move(preName + siteType, "WorkingDir/" +
+         preName + '/' + preName + siteType)
     move(preName + "_temp.csv", "WorkingDir/" +
-        preName + '/' + preName + "_temp.csv")
+         preName + '/' + preName + "_temp.csv")
 
+# End Animation.
 done = True
 print ("\nDing! Job Done!")
