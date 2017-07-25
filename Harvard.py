f0d7b86c69e96e44e27b6e1bb3b79f3b537fdfc8
James Duarte <GarnetSunset@users.noreply.github.com>
2017-07-25 12:28:44 -0400

    Forgot to remove that.

--- Harvard.py
+++ Harvard.py
@@ -12,19 +12,3 @@ def enumColumn(fileName, worksheet):
         for index_col, data_in_col in enumerate(content):
             for index_row, data_in_cell in enumerate(data_in_col):
                 worksheet.write(index_col, index_row, data_in_cell)
-
-# Convert Excel to CSV
-
-
-def Excel2CSV(ExcelFile, SheetName, CSVFile):
-    import csv
-    import xlrd
-    workbook = xlrd.open_workbook(ExcelFile)
-    worksheet = workbook.sheet_by_name(SheetName)
-    csvfile = open(CSVFile, 'wb')
-    wr = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-    for rownum in xrange(worksheet.nrows):
-        wr.writerow(
-            list(x.encode('utf-8') if type(x) == type(u'') else x
-                 for x in worksheet.row_values(rownum)))
-    csvfile.close()
