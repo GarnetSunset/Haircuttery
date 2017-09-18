0c8ffda64011f27a558043a3deb5ba99f75d9f09
James Duarte <GarnetSunset@users.noreply.github.com>
2017-09-18 15:01:24 -0400

    Nailed it. ChromeDriver is now Automatic. ♪~ ᕕ(ᐛ)ᕗ

--- requirements.bat
+++ requirements.bat
@@ -1,4 +1,10 @@
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi', 'VCForPython27.msi')"
 VCForPython27.msi
+del VCForPython27.msi
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://chromedriver.storage.googleapis.com/2.32/chromedriver_win32.zip', 'chromedriver_win32.zip')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('http://stahlworks.com/dev/unzip.exe', 'unzip.exe')"
+unzip chromedriver_win32.zip
+del chromedriver_win32.zip
+del unzip.exe
 easy_install pip
 pip install -q -r requirements.txt
