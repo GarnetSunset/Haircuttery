f8dabd70f7754e5f41dd1665f84b0432f31d1cbf
James Duarte <GarnetSunset@users.noreply.github.com>
2017-09-27 17:08:11 -0400

    Fix Dateutil

--- requirements.bat
+++ requirements.bat
@@ -11,3 +11,4 @@ del unzip.exe
 del python-2.7.msi
 C:\Python27\Scripts\easy_install.exe pip
 C:\Python27\Scripts\pip.exe install -q -r requirements.txt
+C:\Python27\Scripts\pip.exe install python-dateutil --upgrade
