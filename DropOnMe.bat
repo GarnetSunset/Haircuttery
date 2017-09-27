f78198c1f9400ef5dcc54b7c6474925a1a6a90da
James Duarte <GarnetSunset@users.noreply.github.com>
2017-09-27 17:36:25 -0400

    Fixed droponme

--- DropOnMe.bat
+++ DropOnMe.bat
@@ -8,11 +8,17 @@ python "FileSplit.py" %*
 
 cls
 
-if exist "tempSmall.log" (goto JustGoForIt)
+if exist "tempFile.log" (
 
 set /p folderName=<"tempFile.log"
 
-if exist "tempFile.log" (del /f "tempFile.log")
+del /f "tempFile.log")
+
+if exist "tempSmall.log" (
+
+set /p folderName=<"tempSmall.log"
+
+del /f "tempSmall.log")
 
 cd /d WorkingDir
 
@@ -27,7 +33,7 @@ SET /P webSite=input 4 for ShouldIAnswer, input 5 for YellowPages, or A for All:
 
 :HairCut
 
-set fileName="%folderName%_%COUNTER%.xlsx"
+set fileName="%folderName%.xlsx"
 
 if exist %fileName% (
 
@@ -59,51 +65,8 @@ goto youWin
 
 )
 
-set /A COUNTER=COUNTER+1
-
-goto Haircut
-
 :youWin
 
 cd ..
-
-EXIT /B
-
-:JustGoForIt
-
-set /p folderName=<"tempSmall.log"
-
-del /f "tempSmall.log"
-
-cd /d WorkingDir
-
-cd /d %folderName%
-
-cls
-
-echo Input 1 for whoscall.in results, input 2 for BBB, input 3 for 800Notes,
-SET /P webSite=input 4 for ShouldIAnswer, input 5 for YellowPages, or A for All:
-
-set fileName="%folderName%.xlsx"
-
-if "%webSite%" == "A" (
-
-python "Haircut.py" %fileName% 1
-
-python "Haircut.py" %fileName% 2
-
-python "Haircut.py" %fileName% 3
-
-python "Haircut.py" %fileName% 4
-
-python "Haircut.py" %fileName% 5
-
-python "Haircut.py" %fileName% A
-
-) else (
-
-python "Haircut.py" %fileName% %webSite%
-
-)
 
 EXIT /B
