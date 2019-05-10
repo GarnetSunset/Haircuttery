ae22d0b65b2e44894db1c5e6a3f3bc7500a5bea1
James Duarte <GarnetSunset@users.noreply.github.com>
2019-05-10 16:50:54 -0400

    Update requirements.bat

--- requirements.bat
+++ requirements.bat
@@ -1,8 +1,3 @@
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://chromedriver.storage.googleapis.com/2.40/chromedriver_win32.zip', 'chromedriver_win32.zip')"
-powershell -Command "(New-Object Net.WebClient).DownloadFile('http://stahlworks.com/dev/unzip.exe', 'unzip.exe')"
-unzip chromedriver_win32.zip
-del chromedriver_win32.zip
-del unzip.exe
 C:\Python27\Scripts\easy_install.exe pip
 C:\Python27\Scripts\pip.exe install -q -r requirements.txt
 C:\Python27\Scripts\pip.exe install python-dateutil --upgrade
